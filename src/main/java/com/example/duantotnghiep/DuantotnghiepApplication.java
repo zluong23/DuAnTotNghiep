df6fafbb746e7742e0b99923b247e49c08c34140
Luongpvph27222 <luongpvph27222@fpt.edu.vn>
2023-06-10 23:26:02 +0700

    update

---
+++ src/main/java/com/example/duantotnghiep/DuantotnghiepApplication.java
@@ -0,0 +1,13 @@
+package com.example.duantotnghiep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuantotnghiepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DuantotnghiepApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/duantotnghiep/DuantotnghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.duantotnghiep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuantotnghiepApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
